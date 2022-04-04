875a213e774f577078d744be8802bef3d9d57ff3
John Jacobs <john.a.jacobs1973@gmail.com>
2022-04-04 19:05:51 -0400

    replace faker with falso

--- seeders/20210228194043-businesses.js
+++ seeders/20210228194043-businesses.js
@@ -1,13 +1,13 @@
 'use strict'
-const faker = require('faker')
+const falso = require('@ngneat/falso')
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     const businesses = [...Array(100)].map((_) => {
-      const address = `${faker.address.streetAddress()} ${faker.address.city()}, ${faker.address.stateAbbr()}`
+      const address = `${falso.randStreetAddress()} ${falso.randCity()}, ${falso.randState()}`
       return {
-        name: faker.company.companyName(),
+        name: falso.randCompanyName(),
         address: address,
-        rating: faker.datatype.number({ min: 0, max: 5 }),
+        rating: falso.randNumber({ min: 0, max: 5 }),
         createdAt: new Date(),
         updatedAt: new Date()
       }
